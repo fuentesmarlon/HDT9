b5ad716dd0af699d72587fe19ecd3e71b7f6a031
fuentesmarlon <fue15240@uvg.edu.gt>
2016-10-15 17:57:41 -0600

    Add files via upload
    Version 1

---
+++ src/Traductor.java
@@ -0,0 +1,19 @@
+package org.uvg.utilidades;
+
+public class Traductor {
+	
+	public void traducirTexto(String texto){
+		
+		
+		String [] palabras = texto.split(" ");
+		String [] palabrasTraducidas;
+		
+		for(String e:palabras){
+			
+			
+			System.out.println(e);
+			
+		}
+		
+	}
+}
