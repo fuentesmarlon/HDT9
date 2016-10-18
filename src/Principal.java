246066a5c06e95072f8c9d040f3807c49e359b53
fuentesmarlon <fue15240@uvg.edu.gt>
2016-10-17 21:10:04 -0600

    Add files via upload

--- src/Principal.java
+++ src/Principal.java
@@ -17,12 +17,22 @@ public class Principal {
     public static void main(String[] args) {
         
         Scanner scan = new Scanner(System.in);
-        System.out.println("Bienvenido"+"\nElija la implementacion:"+"\n1) Hash"+"\n2) ");
+        System.out.println("Bienvenido"+"\nElija la implementacion (colocar Hash o ):"+"\n1) Hash"+"\n2) ");
         String operacion = scan.nextLine();
+        Factory fabrica = new Factory();
+        while(!operacion.equals("1") && !operacion.equals("2") && !operacion.equals("Hash")){
+            System.out.println("Bienvenido"+"\nElija la implementacion (colocar Hash o ):"+"\n1) Hash"+"\n2) ");
+            operacion = scan.nextLine();
+        }
         if(operacion.equals("Hash")||operacion.equals("1")){
-            Map<String,String> elHash = new HashMap();
+           
             
         }
+        if(operacion.equals("")|| operacion.equals("2")){
+            
+        }
+       
+            
             
         
     
--- src/Utilidades.java
+++ src/Utilidades.java
@@ -16,38 +16,28 @@ import java.util.List;
  * @author Marlon
  */
 public class Utilidades {
-    public void lector(){
-           List<String> ingles = new ArrayList<String>();
-        List<String> espanol = new ArrayList<String>();
-        BufferedReader Br = null;
+          private ArrayList<String> array = new ArrayList<String>();
+          private ArrayList<String> arrayTraducido = new ArrayList<String>();
+             
+    public ArrayList<String>  lector(String archivo){ 
         try{
-            ArrayList<String> diccionario = new ArrayList<>();
-            String line;
-            String[] traduccion;
-             Br = new BufferedReader(new FileReader(""));
-             while (true){
-                 line = Br.readLine();
-                 if(line == null){
-                     break;
-                 }
-                 else{
-                     traduccion = line.split("[\\\\(\\\\)]");
-                     for(String cada :traduccion){
-                         if(!"".equals(cada)){
-                             diccionario.add(cada);
-                         }
-                     }
-                     for (String each :diccionario){
-                         String inglesPalabra = diccionario.get(0);
-                         String espanolPalabra = diccionario.get(1);
-                         ingles.add(inglesPalabra);
-                         espanol.add(espanolPalabra);
-                     }
-                 }
-             }
-        }catch(IOException e){
+            String linea;
+            BufferedReader Br = null;
+            Br = new BufferedReader(new FileReader(""));
+            while((linea = Br.readLine())!= null){
+               array.add(linea);
+           }
+    
+
+        }
+    catch(IOException e){
             System.out.println("Archivo no encontrado");
             
         }
+        return array;
     }
+   
+    
+    
 }
+
